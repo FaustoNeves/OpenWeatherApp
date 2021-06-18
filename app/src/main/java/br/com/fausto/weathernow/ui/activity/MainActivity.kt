9afeafe953d6bb753bfd366a5d170a808eac12a8
Fausto Neves Kina <fausto.kina@cromai.com>
2021-06-18 14:04:23 -0300

    Layout changed. Volley removed, replaced for retrofit. All data work is done (apparently). Last part: stylization

--- app/src/main/java/br/com/fausto/weathernow/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/fausto/weathernow/ui/activity/MainActivity.kt
@@ -1,37 +1,32 @@
 package br.com.fausto.weathernow.ui.activity
 
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import br.com.fausto.weathernow.R
+import br.com.fausto.weathernow.databinding.ActivityMainBinding
 import br.com.fausto.weathernow.ui.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textCity: EditText
-    private lateinit var textCountry: EditText
-    private lateinit var textResult: TextView
     private val weatherViewModel: WeatherViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        textCity = findViewById(R.id.city)
-        textCountry = findViewById(R.id.country)
-        textResult = findViewById(R.id.result)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = weatherViewModel
+        binding.lifecycleOwner = this
 
-        weatherViewModel.city.observe(this, {
-            textResult.text = it
+        weatherViewModel.message.observe(this, {
+            it.getContentIfNotHandled()?.let { message ->
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            }
         })
-    }
-
-    fun getWeather(view: View) {
-        weatherViewModel.getWeather(textCity.text.toString())
     }
 }
--- app/src/main/java/br/com/fausto/weathernow/ui/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/br/com/fausto/weathernow/ui/viewmodel/WeatherViewModel.kt
@@ -1,33 +1,87 @@
 package br.com.fausto.weathernow.ui.viewmodel
 
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.fausto.weathernow.data.repository.WeatherRepository
+import br.com.fausto.weathernow.ui.utils.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WeatherViewModel @Inject constructor(private val weatherRepository: WeatherRepository) :
-    ViewModel() {
+    ViewModel(), Observable {
 
-    private val _city = MutableLiveData<String>()
-    val city: LiveData<String> = _city
+    private val statusMessage = MutableLiveData<Event<String>>()
+    val message: LiveData<Event<String>>
+        get() = statusMessage
 
-    private val _country = MutableLiveData<String>()
-    val country: LiveData<String> = _country
+    @Bindable
+    val cityName = MutableLiveData<String>()
 
-    fun getWeather(cityName: String) {
-        viewModelScope.launch {
-            try {
-                val weatherResult = weatherRepository.getWeather(cityName)
-                _city.value = weatherResult.name!!
-                _country.value = weatherResult.sys!!.country!!
-            } catch (exception: Exception) {
-                //Error
+    @Bindable
+    val countryCode = MutableLiveData<String>()
+
+    @Bindable
+    val displayCityName = MutableLiveData<String>()
+
+    @Bindable
+    val displayCountryName = MutableLiveData<String>()
+
+    @Bindable
+    val displayTemperature = MutableLiveData<String>()
+
+    @Bindable
+    val minTemp = MutableLiveData<String>()
+
+    @Bindable
+    val maxTemp = MutableLiveData<String>()
+
+    @Bindable
+    val humidity = MutableLiveData<String>()
+
+    fun getWeather() {
+        if ((cityName.value == null) && (countryCode.value == null)) {
+            statusMessage.value = Event("At least one parameter is required")
+        } else {
+            val searchValue: String? =
+                if ((countryCode.value == null) && (cityName.value != null)) {
+                    cityName.value
+                } else if ((countryCode.value != null) && (cityName.value == null)) {
+                    countryCode.value
+                } else {
+                    cityName.value + "," + countryCode.value
+                }
+            viewModelScope.launch {
+                try {
+                    val weatherResult = weatherRepository.getWeather(searchValue!!)
+                    displayCityName.value = weatherResult.name
+                    displayCountryName.value = weatherResult.sys!!.country
+                    displayTemperature.value = (weatherResult.main!!.temp!! - 273.15).toString()
+                    minTemp.value = (weatherResult.main!!.temp_min!! - 273.15).toString()
+                    maxTemp.value = (weatherResult.main!!.temp_max!! - 273.15).toString()
+                    humidity.value =
+                        "Humidity:" + (weatherResult.main!!.humidity!!).toString() + "%"
+                } catch (exception: Exception) {
+                    statusMessage.value = Event("Something went bad")
+                }
+                clearFields()
             }
         }
+    }
+
+    private fun clearFields() {
+        cityName.value = ""
+        countryCode.value = ""
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
     }
 }
