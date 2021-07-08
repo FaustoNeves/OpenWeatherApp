e455bed30c0ec115b7a79835a43e761cd248bea5
Fausto Neves Kina <fausto.kina@cromai.com>
2021-07-08 15:42:52 -0300

    Lottie animations added for weathers and day/night check. Its still requires a proper background for both day and night

--- app/src/main/java/br/com/fausto/weathernow/ui/fragment/WeatherFragment.kt
+++ app/src/main/java/br/com/fausto/weathernow/ui/fragment/WeatherFragment.kt
@@ -36,6 +36,41 @@ class WeatherFragment : Fragment() {
 
         splashViewModel.weather.observe(viewLifecycleOwner, { weather ->
             bindingFragment.let {
+                weather.weather!![0].let {
+                    if (it.main.equals("Thunderstorm")) {
+                        if (checkLastCharacterForD(it.icon)!!) {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.heavy_rain)
+                        } else {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.heavy_rain)
+                        }
+                    } else if (it.main.equals("Drizzle") or it.main.equals("Rain")) {
+                        if (checkLastCharacterForD(it.icon)!!) {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.day_rain)
+                        } else {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.night_rain)
+                        }
+                    } else if (it.main.equals("Snow")) {
+                        if (checkLastCharacterForD(it.icon)!!) {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.day_snow)
+                        } else {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.night_snow)
+                        }
+                    } else if (it.main.equals("Clear")) {
+                        if (checkLastCharacterForD(it.icon)!!) {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.day_clean)
+                        } else {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.night_clean)
+                        }
+                    } else if (it.main.equals("Clouds")) {
+                        if (checkLastCharacterForD(it.icon)!!) {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.day_cloud)
+                        } else {
+                            bindingFragment.weatherAnimation.setAnimation(R.raw.night_cloud)
+                        }
+                    } else {
+                        bindingFragment.weatherAnimation.setAnimation(R.raw.mist)
+                    }
+                }
                 regionName.text = weather.name
                 weatherStatus.text = weather.weather!![0].description
                 currentWeatherText.text = decimalToCelsius(weather.main?.temp)
@@ -63,4 +98,7 @@ class WeatherFragment : Fragment() {
         val df = DecimalFormat("#")
         return df.format(doubleText?.minus(273.15)).toString() + "°C"
     }
+
+    private fun checkLastCharacterForD(stringSequence: String?) = stringSequence?.endsWith("d")
+
 }
